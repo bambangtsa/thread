b52ea97d26660ecc8f4b19043e1a2a28bd4b07a6
bambangtsa <63002866+bambangtsa@users.noreply.github.com>
2020-04-01 22:29:23 +0700

    Add files via upload

---
+++ MethodSinkronisasi.java
@@ -0,0 +1,31 @@
+public class MethodSinkronisasi extends Thread
+{
+	public MethodSinkronisasi(String name)
+	{
+		super(name);
+	}
+	public static synchronized void tampil(String nama)
+	{
+		for(int i=0;i<5;i++)
+		{
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("Thread: "+nama+" posisi: "+i);
+		}
+	}
+	public void run()
+	{
+		tampil(getName());
+	}
+	public static void main(String args[])
+	{
+		Thread odong = new Thread(new MethodSinkronisasi("odong"));
+		Thread andong = new Thread(new MethodSinkronisasi("andong"));
+		odong.start();
+		andong.start();
+	}
+}
